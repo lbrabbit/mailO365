b7da9a7338be293982d20514c276bd2d2d6d8867
Haggen So <haggenso@gmail.com>
2018-12-02 20:56:47 +0800

    First working files

--- SendersinCSV/SendersinCSV.py
+++ SendersinCSV/SendersinCSV.py
@@ -1,7 +1,34 @@
+import sys
+sys.path.append('..')
 import pandas as pd
+import faddr
+import getpass
+import myemailib
 
 df = pd.read_csv("GroupLeader.csv",index_col=0)
+fromaddr = faddr.faddr
+passwd = getpass.getpass('Password: ')  
 for index, row in df.iterrows():
-   print (index, row['Grp'], row['Name'])
+  print (index+"@student.hkcc-polyu.edu.hk", "%02d" % row['Grp'], row['Name'])
 
+  toaddr = index+"@student.hkcc-polyu.edu.hk"
+  body = "Dear Group Leader "+row['Name']+","
+  body += """
 
+Please find the attached mark sheet for your group project and inform your group members.
+
+Regards,
+
+Haggen"""
+  directory = "./pdf"
+  filename = "CCN1031_Group_Project_Marking_%02d.xls.pdf" % row['Grp']
+
+  server = myemailib.email_login(fromaddr, passwd)
+  msg = myemailib.create_msg(fromaddr, toaddr, "CCN1031 Group Project Mark", body)
+  myemailib.attach_msg(directory, filename, msg)
+
+  text = msg.as_string()
+  server.sendmail(fromaddr, toaddr, text)
+  input("Press Enter to continue...")
+
+server.quit()
