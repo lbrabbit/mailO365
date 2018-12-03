72e0cd57e7567fb63fa1998ff63ef0f573884089
Haggen So <haggenso@gmail.com>
2018-12-03 22:50:28 +0800

    Add Delay and Comments

--- SendersinCSV/SendersinCSV.py
+++ SendersinCSV/SendersinCSV.py
@@ -1,34 +1,41 @@
 import sys
-sys.path.append('..')
 import pandas as pd
-import faddr
 import getpass
-import myemailib
+import time
 
-df = pd.read_csv("GroupLeader.csv",index_col=0)
+# Import my libraries
+sys.path.append('..')
+import myemailib
+import faddr
+
+df = pd.read_csv("A2.csv",index_col=0)
+# My email is hidden in faddr.py, replace with your email below
 fromaddr = faddr.faddr
 passwd = getpass.getpass('Password: ')  
 for index, row in df.iterrows():
-  print (index+"@student.hkcc-polyu.edu.hk", "%02d" % row['Grp'], row['Name'])
+  print (index+"@student.hkcc-polyu.edu.hk")
 
+  # For Testing
+  # toaddr = fromaddr
   toaddr = index+"@student.hkcc-polyu.edu.hk"
-  body = "Dear Group Leader "+row['Name']+","
+  body = "Dear Student"+","
   body += """
 
-Please find the attached mark sheet for your group project and inform your group members.
+Please find the attached mark sheet for your Access In-Class Exercise. Also, the group project mark sheet was delivered to your group leader.
 
 Regards,
 
 Haggen"""
   directory = "./pdf"
-  filename = "CCN1031_Group_Project_Marking_%02d.xls.pdf" % row['Grp']
+  filename = index+" (Marking) Access CCN1031.pdf"
 
   server = myemailib.email_login(fromaddr, passwd)
-  msg = myemailib.create_msg(fromaddr, toaddr, "CCN1031 Group Project Mark", body)
+  msg = myemailib.create_msg(fromaddr, toaddr, "CCN1031 Access In-Class Mark", body)
   myemailib.attach_msg(directory, filename, msg)
 
   text = msg.as_string()
   server.sendmail(fromaddr, toaddr, text)
-  input("Press Enter to continue...")
+  #input("Press Enter to continue...")
+  time.sleep(2)
 
 server.quit()
